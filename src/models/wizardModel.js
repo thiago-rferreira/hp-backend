610f782931c460ae1d7f1d516cce23850a7adcad
Thiago Ferreira <thiago.rodrigues@docente.senai.br>
2025-03-31 14:20:59 -0300

    comentario

--- src/models/wizardModel.js
+++ src/models/wizardModel.js
@@ -1,6 +1,18 @@
 const pool = require("../config/database");
 
 const getWizards = async () => {
+    /*
+    wizards.* → Pega todas as colunas da tabela wizards.
+
+    houses.name AS house_name → Seleciona o nome da casa e dá a ela um alias house_name.
+
+    FROM wizards → Define a tabela principal (wizards), onde buscamos os dados dos bruxos.
+
+    LEFT JOIN houses ON wizards.house_id = houses.id → Junta a tabela houses com wizards, 
+    ligando house_id da tabela wizards ao id da tabela houses. 
+    O LEFT JOIN garante que mesmo bruxos sem casa cadastrada ainda apareçam na consulta.
+
+    */
     const result = await pool.query(
         `SELECT wizards.*, houses.name AS house_name 
          FROM wizards 
